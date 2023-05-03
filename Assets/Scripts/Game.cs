058199a095276a4c5f6df0855a735033aa5cbdd0
gulernurdonmez <gule0153@stud.kea.dk>
2023-05-03 11:46:27 +0200

    mase esc funtionality

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game : MonoBehaviour
 {
-
     public AudioSource audioSource;
-
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +17,9 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
     }
 }

666ecf1eff2ec21042a076b15486c305ceb00ae9
gulernurdonmez <gule0153@stud.kea.dk>
2023-05-03 12:54:00 +0200

    added timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+public class Timer : MonoBehaviour {
+
+    public TMP_Text timerText;
+    float startTime;
+    bool finished = false;
+    string seconds;
+    float t;
+
+	// Use this for initialization
+	void Start () {      
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (finished)
+            return;
+
+        t = Time.time + startTime;
+
+        seconds = (t % 60).ToString("f2");
+
+        timerText.text = seconds;
+	}
+
+    public void Finish()
+    {
+        if (t == 5.00f)
+        {
+            finished = true;
+            timerText.color = Color.green;
+        }
+        else if (t > 5.00f)
+        {
+            finished = true;
+            timerText.color = Color.red;
+        }
+        else if(t < 5.00f)
+        {
+            finished = true;
+            timerText.color = Color.red;
+        }
+    }
+}

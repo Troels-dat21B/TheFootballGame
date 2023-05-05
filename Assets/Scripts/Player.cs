f13b351754d4727732607d08c3f92e1b5ce33a6a
Troels-dat21B <troe1912@stud.kea.dk>
2023-05-05 17:19:36 +0200

    removed unnecessary out-commented code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -273,24 +273,7 @@ public class Player : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// OnCollisionEnter is called when this collider/rigidbody has begun
-    /// touching another rigidbody/collider.
-    /// </summary>
-    /// <param name="other">The Collision data associated with this collision.</param>
 
-/*     void OnCollisionEnter(Collision other)
-    {
-
-        if (other.gameObject.tag == "Ball")
-        {
-            other.gameObject.GetComponent<Ball>().IsWithPlayer = true;
-            //other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-            other.gameObject.GetComponent<Rigidbody>().useGravity = false;
-            Debug.Log("Ball is with player");
-            //other.gameObject.transform.position = BallPlacement.position;
-        }
-    } */
 
     /// <summary>
     /// OnControllerColliderHit is called when the controller hits a

e049415b4e6a95d8d6d2ee53c06d345343ab3474
Troels-dat21B <troe1912@stud.kea.dk>
2023-05-10 14:08:55 +0200

    Removed ChatGPT example, since it didnt work

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,9 +7,8 @@ using UnityEngine.SceneManagement;
 
 
 
-//This is taken from this link: https://forum.unity.com/threads/start-timer-on-new-scene.487709/
 
-/* public class Timer : MonoBehaviour
+public class Timer : MonoBehaviour
 {
 
     public TMP_Text timerText;
@@ -66,50 +65,6 @@ using UnityEngine.SceneManagement;
             timerText.color = Color.red;
         }
     }
-} */
-public class Timer : MonoBehaviour
-{
-    public TMP_Text timerText;
-    float startTime;
-    bool finished = false;
-    string seconds;
-    float t;
-
-    void Awake()
-    {
-        timerText.text = "0"; // set initial value to "0"
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (finished)
-            return;
-
-        t = Time.time + startTime;
-
-        seconds = (t % 60).ToString("f2");
-
-        timerText.text = seconds;
-    }
-
-    public void Finish()
-    {
-        if (t == 5.00f)
-        {
-            finished = true;
-            timerText.color = Color.green;
-        }
-        else if (t > 5.00f)
-        {
-            finished = true;
-            timerText.color = Color.red;
-        }
-        else if (t < 5.00f)
-        {
-            finished = true;
-            timerText.color = Color.red;
-        }
-    }
 }
+
 
